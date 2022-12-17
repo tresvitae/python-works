d43b7c993789d56682c0c44416c71f1a6d8745c9
tresvitae <patryk.futa@gmail.com>
2022-12-17 20:32:32 +0100

    Create inital commit method

--- api/APIrequests.py
+++ api/APIrequests.py
@@ -1,5 +1,6 @@
 import requests
 from os import environ
+import base64
 
 api_token = environ['GITHUB_API_TOKEN']
 
@@ -34,6 +35,25 @@ class GitHubClient:
             print(e)
             return None
 
+    def create_initial_commit(self, owner: str, repo: str, branch: str, message: str, content: str):
+        """
+        Create initial commit on the new repository.
+        """
+        url = f'{self.base_url}repos/{owner}/{repo}/contents/README.md'
+        data = {
+            'message': message,
+            'content': content#,
+            #'branch': branch
+        }
+
+        try:
+            response = requests.put(url, json=data, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return None
+        
     def get_branch_sha(self, owner: str, repo: str, branch: str):
         """
         Get the SHA of the latest commit on the specified branch.
@@ -90,7 +110,7 @@ class GitHubClient:
 client = GitHubClient(api_token)
 
 #Create the repository 
-repo_name = "my-peex-test-repo7"
+repo_name = "my-peex-test-repo5"
 
 response = client.create_repository(repo_name)
 
@@ -99,38 +119,40 @@ if response:
 else:
     print('Failed to create repository.')
 
-#Get latest sha and create new branch
-original_branch_name = "main"
-new_branch_name = "developer"
+# Create the initial commit in the new repository
 owner_name = "tresvitae"
+original_branch_name = "master"
+message = 'Initial commit'
+
+#Encode to Base64 fromat
+encode_test = base64.b64encode(b'PEEX I WANNA GET PROMOTION')
+content = encode_test.decode()
+
+response = client.create_initial_commit(owner_name, repo_name, original_branch_name, message, content)
+
+if response:
+    print(f'New commit with message {base64.b64decode(content)} created successfully!.')
+else:
+    print('Failed to create initial commit.')
+
+#Get latest sha and create new branch
+new_branch_name = "developer"
 
 sha_id = client.get_branch_sha(owner_name, repo_name, original_branch_name)
 response = client.create_branch(owner_name, repo_name, new_branch_name, sha_id)
-print(sha_id)
+
 if response:
     print(f'New branch {new_branch_name} created successfully from {sha_id}.')
 else:
     print('Failed to create the branch.')
 
-
 #Put the tag on the initial commit of the new branch
 tag_name = "v1.0.0"
+
 response = client.create_tag(owner_name, repo_name, sha_id, tag_name)
 
-print(sha_id)
 if response:
     print(f'Tag {tag_name} added successfully!')
 else:
     print(f'Failed to add tag to initial commit in {new_branch_name} branch.')
 
-
-
-'''
-echo f'# {repo_name}' >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/tresvitae/my-peex-test-repo7.git
-git push -u origin main
-'''
